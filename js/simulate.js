cea28285f550daa9d9911cdd0f0b9fbc84efa52e
bsse1006 <bsse1006@iit.du.ac.bd>
2021-10-18 04:58:00 +0600

    AI Base 3.1

--- js/simulate.js
+++ js/simulate.js
@@ -7,37 +7,47 @@ class Simulate {
         isManualMode = false;
         var ai = new Ai (this.world);
         let nextMoveArray = [];
+        let wumpuskillingwaiter = 0;
         interval = setInterval(()=>{
             if (nextMoveArray.length==1&&ai.killWumpus==true)
             {
                 this.world.agent.direction=nextMoveArray[0];
                 this.world.agent.shoot();
                 ai.killWumpus = false;
+                wumpuskillingwaiter=3;
             }
-            if (nextMoveArray.length==0)
+            else if (wumpuskillingwaiter==0)
             {
-                nextMoveArray = ai.getNextMove();
+                if (nextMoveArray.length==0)
+                {
+                    nextMoveArray = ai.getNextMove();
+                }
+
+                if (nextMoveArray[0]==0)
+                {
+                    this.world.agent.up();
+                }
+                else if (nextMoveArray[0]==1)
+                {
+                    this.world.agent.right();
+                }
+                else if (nextMoveArray[0]==2)
+                {
+                    this.world.agent.down();
+                }
+                else if (nextMoveArray[0]==3)
+                {
+                    this.world.agent.left();
+                }
+
+                nextMoveArray.shift();
             }
 
-            if (nextMoveArray[0]==0)
+            if (wumpuskillingwaiter>0)
             {
-                this.world.agent.up();
-            }
-            else if (nextMoveArray[0]==1)
-            {
-                this.world.agent.right();
-            }
-            else if (nextMoveArray[0]==2)
-            {
-                this.world.agent.down();
-            }
-            else if (nextMoveArray[0]==3)
-            {
-                this.world.agent.left();
+                wumpuskillingwaiter--;
             }
 
-            nextMoveArray.shift();
-
-        }, 1000);        
+        }, 600);        
     }
 }
